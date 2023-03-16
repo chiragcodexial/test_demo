a6e89a8f2d89f7428d6eb771ba0f2abed951f0a9
unknown <chiragshah.codexial.in>
2023-03-16 10:33:13 +0530

    changes done on the file

--- pythontest.py
+++ pythontest.py
@@ -1,3 +1,4 @@
 print("this is the test for S3 bucket")
 print("If test is success the will pust in git late on")
 print("Test done on git now on s3 bucket is pending")
+print("Test completed and suceeded for the same merged with s3 bucket")
